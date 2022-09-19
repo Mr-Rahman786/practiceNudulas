d70c9e678b39c1483f8851a5674b20c5e563f130
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2022-09-19 10:26:58 +0800

    added const and let

---
+++ differntLetConst.js
@@ -0,0 +1,10 @@
+let firtsValue = 10;
+const secondValue = 10;
+
+function justify() {
+    let firtsValue = 19;
+    console.log(firtsValue);
+    const secondValue = 20;
+    let.log(secondValue);
+}
+justify();
