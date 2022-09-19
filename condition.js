b5ad5fd88347e7e01c8383edc301ac0c61472afd
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2022-09-19 11:46:37 +0800

    added

---
+++ condition.js
@@ -0,0 +1,12 @@
+var numberOne = 20;
+var numberTwo = 18;
+
+if (numberOne<numberTwo && numberOne>numberTwo) {
+    console.log('Sorry sir you cant be entry without id');
+}
+else if (numberOne < numberTwo || numberOne > numberTwo) {
+    console.log('you have been passed');
+}
+else {
+    console.log('Today will be finished');
+}
