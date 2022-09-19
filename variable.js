68920059a62b3893666da64a4d3e64d74c848843
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2022-09-19 09:55:41 +0800

    first commit

---
+++ variable.js
@@ -0,0 +1,10 @@
+var stringName = ['me', 'you', 'she', 'her', 'may', 'might'];
+console.log(stringName);
+
+var numberSelect = [12, 51, 52, 14, 76, 88, 78, 09, 31, 51];
+console.log(numberSelect);
+
+var chcek = true;
+console.log(chcek);
+
+
