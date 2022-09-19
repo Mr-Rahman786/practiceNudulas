ace4011caae44629ea829233758524cfe6ae50d5
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2022-09-19 18:39:03 +0800

    added some

---
+++ Math.max.js
@@ -0,0 +1,8 @@
+function chcekNume(numbers) {
+    let grandMax = Math.max(numbers);
+    return grandMax;
+}
+let numbers = (80, 92, 89, 100);
+console.log(numbers);
+
+
---
+++ product.js
@@ -0,0 +1,6 @@
+function numbers(numOne, numTwo, numThree) {
+    result = numOne * numTwo * numThree;
+    return result;
+}
+const totalNumbers = numbers(12, 30, 80);
+console.log(totalNumbers);
