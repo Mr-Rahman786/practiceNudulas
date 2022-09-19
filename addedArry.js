cc91fab0356bd5837fb807ec27baa2e750d839b0
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2022-09-19 12:51:44 +0800

    added something

---
+++ addedArry.js
@@ -0,0 +1,11 @@
+var miningarry = ['chiken', 'salad', 'rice', 'fish', 'frychiken', 'coke', 'kabab'];
+miningarry.push('lemon juice', 'fruit juice','kulfi');
+
+miningarry.pop()
+
+let length = miningarry.length
+console.log(length);
+
+miningarry[4] = 'frykabab';
+console.log(miningarry);
+
